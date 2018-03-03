05a4c47629624d808063cb4491eaf209204fb4bd
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-03-03 18:22:13 +0000

    Building a burger menu on small screens

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,28 +1,41 @@
 <template>
   <div id='navBar'>
-    <router-link to="/" exact>
-      <div class='data hover nav-button' id='home-button'>
-        WIN RUNS
-      </div>
-    </router-link>
+    <div id='openNavBar' class='row'>
+      <router-link to="/" exact>
+        <div class='data hover nav-button' id='home-button'>
+          WIN RUNS
+        </div>
+      </router-link>
 
-    <router-link to="/runclub" exact>
-      <div class='data hover nav-button' id='run-club-button'>
-        <img src='../assets/icon_run4it.png' class='nav-icon'/>
-      </div>
-    </router-link>
+      <div id='pagesBar' class='row'>
+        <router-link to="/runclub" exact>
+          <div class='data hover nav-button' id='run-club-button'>
+            <img src='../assets/icon_run4it.png' class='nav-icon'/>
+          </div>
+        </router-link>
 
-    <router-link to="/parkrun" exact>
-      <div class='data hover nav-button' id='park-run-home-button'>
-        <img src='../assets/icon_park_run.png' class='nav-icon'/>
-      </div>
-    </router-link>
+        <router-link to="/parkrun" exact>
+          <div class='data hover nav-button' id='park-run-home-button'>
+            <img src='../assets/icon_park_run.png' class='nav-icon'/>
+          </div>
+        </router-link>
 
-    <router-link to="/runs" exact>
-      <div class='data hover nav-button' id='runs-button'>
-        <img src='../assets/icon_runs.png' class='nav-icon'/>
+        <router-link to="/runs" exact>
+          <div class='data hover nav-button' id='runs-button'>
+            <img src='../assets/icon_runs.png' class='nav-icon'/>
+          </div>
+        </router-link>
       </div>
-    </router-link>
+    </div>
+    <div id='burger' v-on:click='expandBurger = !expandBurger'>
+      <img src='../assets/icon_burger.png' class='nav-icon'/>
+      <div class='expanded-burger' v-bind:hidden=expandBurger>
+        <p>Home</p>
+        <p>Run Club</p>
+        <p>Park Run</p>
+        <p>Runs</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,6 +44,7 @@ export default {
   name: 'navBar',
   data () {
     return {
+      expandBurger: true
     }
   },
   methods: {
@@ -39,104 +53,140 @@ export default {
 </script>
 
 <style scoped>
-  #navBar {
-  background-color: slategrey;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  width: auto;
-  z-index: 99;
-  padding: 5px 0;
-  position: relative;
-}
+  #burger {
+    visibility: hidden;
+    position: absolute;
+    top: 7px;
+    right: 10px;
+    z-index: 1px;
+  }
 
-.nav-icon {
-  width: auto;
-  height: 30px;
-  margin: 0 5px;
-}
+  .expanded-burger {
+    visibility: visible;
+    position: absolute;
+    top: 2px;  
+    right: 2px;
+    width: 100px;
+    text-align: right;  
+    background-color: rgba(212, 217, 221, 0.8);
+    padding: 10px;
+  }
 
-.nav-icon:hover {
-  transform: scale(1.1);
-}
+  #openNavBar {
+    display: flex;
+    background-color: slategrey;
+    justify-content: flex-end;
+    padding: 5px 0;
+    position: relative;
+  }
 
-.nav-button {
-  cursor: pointer;
-}
+  #pagesBar {
+    display: flex;
+    visibility: visible;
+    justify-content: space-around;
+    width: 300px;
+    padding: 0 20px;
+  }
 
-.button-active {
-  background-color: #3E9FDF;
-  transform: scale(1.1);
-  padding: 0 7px;
-  border-radius: 5px;
-}
+  .row {
+    flex-direction: row;
+  }
 
-.hover:hover {
-  color: #3E9FDF;
-}
+  .col {
+    flex-direction: column;  
+  }
 
-#run-club-button {
-  margin-left: 75px;
-}
+  .nav-icon {
+    width: auto;
+    height: 30px;
+    margin: 0 5px;
+  }
 
-#home-button {
-  font-family: sans-serif;
-  color: transparent;
-  font-weight: lighter;
-  position: absolute;
-  padding: 3px;
-  padding-left: 10px;
-  top: 0px;
-  left: -5px;
-  font-size: 18px;
-  line-height: 90%;
-  word-spacing: 100vw;
+  .nav-icon:hover {
+    transform: scale(1.1);
+  }
 
-  animation-name: title-settle;
-  animation-duration: 0.5s;
-  animation-delay: 2s;
-  animation-fill-mode: forwards;
-}
+  .nav-button {
+    cursor: pointer;
+  }
 
-@keyframes title-settle {
-  from {
-    width: 0px;
+  .button-active {
+    background-color: #3E9FDF;
+    transform: scale(1.1);
+    padding: 0 7px;
+    border-radius: 5px;
+  }
+
+  .hover:hover {
+    color: #3E9FDF;
+  }
+
+  #home-button {
+    font-family: sans-serif;
     color: transparent;
+    font-weight: lighter;
+    position: absolute;
+    padding: 3px;
+    padding-left: 10px;
+    top: 4px;
+    left: -5px;
+    font-size: 18px;
+    line-height: 90%;
+    word-spacing: 100vw;
+
+    animation-name: title-settle;
+    animation-duration: 0.5s;
+    animation-delay: 1s;
+    animation-fill-mode: forwards;
   }
-  to {
-    width: 60px;
-    color: white;
-    background-color: rgba(31, 97, 141, 0.7);
-    border-top-right-radius: 2px;
-    border-bottom-right-radius: 2px;
+
+  @keyframes title-settle {
+    from {
+      width: 0px;
+      color: transparent;
+    }
+    to {
+      width: 60px;
+      color: white;
+      background-color: rgba(31, 97, 141, 0.7);
+      border-top-right-radius: 2px;
+      border-bottom-right-radius: 2px;
+    }
   }
-}
 
-.splits-button {
-  position: absolute;
-  display: flex;
-  align-items: center;
-  top: 50px;
-  right: 5px;
-  cursor: pointer;
-  font-size: 12px;
-  z-index: 0;
-}
+  .splits-button {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    top: 50px;
+    right: 5px;
+    cursor: pointer;
+    font-size: 12px;
+    z-index: 0;
+  }
 
-.graphs-button {
-  right: 40px;
-}
+  .graphs-button {
+    right: 40px;
+  }
 
-.button-icon {
-  width: auto;
-  height: 25px;
-  margin: 0 5px;
-}
+  .button-icon {
+    width: auto;
+    height: 25px;
+    margin: 0 5px;
+  }
 
 /* responsive css  */
-@media all and (max-width: 400px) {
-  #title{
+@media all and (max-width: 600px) {
+  #title {
      font-size: 16px;
+  }
+
+  #burger {
+    visibility: visible;
+  }
+
+  #pagesBar {
+    visibility: hidden;
   }
 }
 </style>
