96f4a995232cb91e2ec0076b548c21c97d89ed36
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-03-07 21:56:21 +0000

    adding in week activities

---
+++ src/components/weekActivities.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id='week-activities'>
+    <div id="week-header">
+      <div onclick="changeWeek(1)" ><strong><</strong></div>
+        <div class="heading" id="dateInView"></div>
+      <div onclick="changeWeek(-1)" id="previous"><strong>></strong></div>
+    </div>
+
+    <div id='week-container'>
+      <div v-for="day in daysOfWeek" :key="day" class='day-bar'>
+        <div class='day-title'>
+          {{ (day.substr(0, 3)).toUpperCase() }}
+        </div>
+        <div class='day-content'>
+          something
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'weekActivities',
+  components: {
+  },
+  data () {
+    return {
+      daysOfWeek: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ]
+    }
+  },
+  created() {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped>
+  #week-activities {
+    bottom: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+
+  #week-header{
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px;
+    height: 40px;
+  }
+
+  .day-bar {
+    display: flex;
+    flex-direction: row;
+    width: 90vw;
+    align-items: center;
+    padding: 10px 0;
+  }
+
+  .day-title {
+    background-color: rgba(31, 97, 141, 0.7);
+    padding: 10px 0;
+    margin-left: 0px;
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
+    display: flex;
+    flex-direction: row;
+    color: white;
+
+    animation-name: day-settle;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    width: 50px;
+    height: 25px;
+    align-content: center;
+    align-items: center;
+  }
+
+
+  @keyframes day-settle--active {
+    from {
+      width: 37px; 
+      color: transparent
+    }
+    to {
+      width: 79%;
+      color: white;
+    }
+  }
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,18 +1,21 @@
 <template>
   <div id='home'>
     <nav-bar></nav-bar>
+    <week-activities></week-activities>
     <stat-bar></stat-bar>
   </div>
 </template>
 
 <script>
 import navBar from "../components/navBar.vue";
+import weekActivities from "../components/weekActivities.vue";
 import statBar from "../components/statBar.vue";
 
 export default {
   name: "home",
   components: {
     "nav-bar": navBar,
+    "week-activities": weekActivities,
     "stat-bar": statBar
   },
   data() {
@@ -24,5 +27,9 @@ export default {
 </script>
 
 <style scoped>
-
+  #home {
+    background-color: rgb(232, 235, 240);
+    height: 100vh;
+    width: 100vw;
+  }
 </style>
