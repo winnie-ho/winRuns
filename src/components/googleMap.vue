45566300a8e17390864d4d5c12083bf734dd64fa
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-03-10 23:25:51 +0000

    messing with google map to fix lag

--- src/components/googleMap.vue
+++ src/components/googleMap.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class='google-map' v-bind:id="mapName">
-  </div>
+  <div class='google-map' v-bind:id='mapName'></div>
 </template>
 
 <script>
 export default {
   name: 'googleMap',
-  props: {
-    name: {
-      type: String,
-      required: true
-    }
-  },
+  props: [ 'name', 'mapActivity' ],
   components: {
   },
   data () {
@@ -19,34 +13,35 @@ export default {
       mapName: this.name + "-map"
     }
   },
-  created() {
-  },
-  mounted: function () {
-    const mapContainer = document.getElementById(this.mapName)
-    const options = {
-      zoom: 13,
-      center: new google.maps.LatLng(this.activity.start_latlng[0], this.activity.start_latlng[1])
-    }
-    const map = new google.maps.Map(mapContainer, options);
-
-    const line = new google.maps.Polyline({
-      path: google.maps.geometry.encoding.decodePath(this.activity.map.polyline),
-      geodesic: true,
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 2,
-      map: map
-    });
-
-    line.setMap(null);
-    line.setMap(map);
-    // map.setCenter(); 
+  mounted() {
+    console.log("MAP ACT", this.mapActivity)
+    if (!this.mapActivity) return
+    this.renderMap()
   },
   methods: {
+    renderMap: function() {
+      const mapContainer = document.getElementById(this.mapName)
+      const coords = this.mapActivity.start_latlng
+      const options = {
+        zoom: 13,
+        center: new google.maps.LatLng(coords[0], coords[1])
+      }
+      const map = new google.maps.Map(mapContainer, options)
+
+      const line = new google.maps.Polyline({
+        path: google.maps.geometry.encoding.decodePath(this.mapActivity.map.polyline),
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: map
+      });
+
+      line.setMap(null);
+      line.setMap(map);
+      // map.setCenter(); 
   },
   computed: {
-    activity: function () {
-      return this.$store.state.activity;
     }
   }
 }
