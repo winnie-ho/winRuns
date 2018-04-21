08a0888ee464d22360ec1d655eea77f036ac7756
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-04-21 08:22:55 +0100

    Adding a auth page

---
+++ src/pages/auth/auth.vue
@@ -0,0 +1,35 @@
+<template src='./auth.html'></template>
+<style scoped src='./auth.css'></style>
+
+<script>
+import navBar from '../../components/navBar/navBar.vue'
+import renderData from '../../mixins/renderData.js'
+
+export default {
+  name: 'auth',
+  components: {
+    'nav-bar': navBar,
+  },
+  mixins: [ renderData ],
+  data () {
+    return {
+      pageTitle: 'LOGIN',
+    }
+  },
+  mounted () {
+    
+    },
+
+  created() {
+    },
+
+  methods: {
+    
+    },
+  computed: {
+
+  }
+}
+</script>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '../pages/home/home.vue'
+import auth from '../pages/auth/auth.vue'
 import runClub from '../pages/runClub/runClub.vue'
 import parkRun from '../pages/parkRun/parkRun.vue'
 import activities from '../pages/activities/activities.vue'
@@ -13,6 +14,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     { path: '/', component: home },
+    { path: '/auth', component: auth },
     { path: '/runClub', component: runClub },
     { path: '/parkRun', component: parkRun },
     { path: '/activities', component: activities },
