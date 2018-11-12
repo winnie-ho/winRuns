8be7371d9f9d402c2b06aacf6fec1ef945616b45
Winnie Ho <winnie-ho@hotmail.co.uk>
2018-11-12 17:01:50 +0000

    Correct day lookup indices.

--- src/mixins/renderData.js
+++ src/mixins/renderData.js
@@ -2,13 +2,13 @@ export default {
   data () {
     return {
       dayLookUp: {
-        0: 'Monday',
-        1: 'Tuesday',
-        2: 'Wednesday',
-        3: 'Thursday',
-        4: 'Friday',
-        5: 'Saturday',
-        6: 'Sunday'
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday'
       },
       monthLookUp: {
         1: 'January',
