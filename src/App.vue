425e7c72387665ec596b9eb5704417881ebc0bca
Winnie Ho <winnie-ho@hotmail.co.uk>
2019-06-09 00:03:50 +0100

    App level animations.

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,33 @@
 </template>
 
 <script>
-import auth from './pages/auth/auth.vue'
-import home from './pages/home/home.vue'
-import swim from './pages/swim/swim.vue'
-import parkRun from './pages/parkRun/parkRun.vue'
-import activities from './pages/activities/activities.vue'
-import parkRunActivities from './pages/parkRunActivities/parkRunActivities.vue'
+import auth from "./pages/auth/auth.vue";
+import home from "./pages/home/home.vue";
+import swim from "./pages/swim/swim.vue";
+import parkRun from "./pages/parkRun/parkRun.vue";
+import activities from "./pages/activities/activities.vue";
+import parkRunActivities from "./pages/parkRunActivities/parkRunActivities.vue";
 
 export default {
   components: {
-    'auth': auth,
-    'home': home,
-    'swim': swim,
-    'parkRun': parkRun,
-    'activities': activities,
-    'parkRunActivities': parkRunActivities
+    auth: auth,
+    home: home,
+    swim: swim,
+    parkRun: parkRun,
+    activities: activities,
+    parkRunActivities: parkRunActivities
   },
-  name: 'App',
-  mounted () {
-    this.$store.dispatch('fetchWeatherNow')
-    this.$store.dispatch('fetchWeatherForecast')
+  name: "App",
+  mounted() {
+    this.$store.dispatch("fetchWeatherNow");
+    this.$store.dispatch("fetchWeatherForecast");
   },
-  methods: {
-  },
-  computed: {
-  }
-}
+  methods: {},
+  computed: {}
+};
 </script>
 
 <style>
-  @import './app-style.css'
+@import "./app-style.css";
+@import "./app-animations.css";
 </style>
