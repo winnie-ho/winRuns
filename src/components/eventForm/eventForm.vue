1bd07ef7aeaaf9472705645565031e4015ba97b9
Winnie Ho <winnie-ho@hotmail.co.uk>
2019-07-11 22:35:37 +0100

    Adding event form

---
+++ src/components/eventForm/eventForm.vue
@@ -0,0 +1,41 @@
+<template src="./eventForm.html"></template>
+<style scoped src='./eventForm.css'></style>
+
+<script>
+import renderData from "../../mixins/renderData.js";
+import changePage from "../../mixins/changePage.js";
+
+export default {
+  name: "eventForm",
+  components: {},
+  mixins: [renderData],
+  props: [],
+  data() {
+    return {
+      form: {
+        title: "",
+        type: "",
+        date: "",
+        time: "",
+        race: false,
+        description: ""
+      }
+    };
+  },
+  methods: {
+    createEvent() {
+      const { date, time } = this.form;
+
+      if (!date) return;
+      this.$store.dispatch("createEvent", {
+        ...this.form,
+        start_date: date
+          .toISOString()
+          .substr(0, 11)
+          .concat(time.toISOString().substr(11, 20))
+      });
+    }
+  },
+  computed: {}
+};
+</script>
