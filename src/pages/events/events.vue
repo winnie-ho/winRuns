1631cd7c3821f4a7025982604cf7179a9cc3e0fd
Winnie HoShing <winnie.hoshing@prudential.co.uk>
2019-07-15 16:59:33 +0100

    Removing redundant function.

--- src/pages/events/events.vue
+++ src/pages/events/events.vue
@@ -48,15 +48,6 @@ export default {
     events() {
       return this.$store.state.events;
     },
-    filteredEvents() {
-      if (!this.events && !this.wordToSearch) return;
-      const filteredEventsByWordSearch = this.events.filter(event => {
-        return event.title
-          .toLowerCase()
-          .includes(this.wordToSearch.toLowerCase());
-      });
-      return this.filterByEvents(filteredEventsByWordSearch);
-    },
     numberOfRaces() {
       return this.$store.state.events.reduce((total, event) => {
         return total + (event.category === true);
