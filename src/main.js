25363ac16d6b2528a33b5fe9a52be9b30ea2595c
Winnie Ho <winnie-ho@hotmail.co.uk>
2019-10-29 20:59:10 +0000

    Removing redundant commented out code.

--- src/main.js
+++ src/main.js
@@ -11,14 +11,6 @@ import ElementUI from 'element-ui'
 import i18n from 'vue-i18n'
 import locale from 'element-ui/lib/locale/lang/en'
 
-// import * as VueGoogleMaps from 'vue2-google-maps'
-
-// Vue.use(VueGoogleMaps, {
-//   load: {
-//     key: 'AIzaSyCxXMhBSrjeLubVwNJzRAFZedtQGdiCqiI'
-//   }
-// })
-
 Vue.use(VueResource)
 Vue.use(xml2js)
 Vue.use(fs)
