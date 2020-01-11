5e791d883f36c0d5ad025af6b15fcfec51f274b3
Winnie Ho <winnie-ho@hotmail.co.uk>
2020-01-11 17:31:46 +0000

    Creating a park run directory.

---
+++ src/mixins/parkRunDirectory.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line import/prefer-default-export
+export const parkRunDirectory = [
+  {
+    name: 'Edinburgh',
+    startCoords: [55.98, -3.29],
+    segmentId: '1531025',
+    segDict: {
+      1: 'Edinburgh park run first km',
+      2: 'Edinburgh Parkrun 2nd Kilometre',
+      3: 'Edinburgh Parkrun 3rd Kilometre',
+      4: 'Edinburgh Parkrun 4th Kilometre',
+      5: 'Edinburgh Parkrun 5th "Kilometre"',
+    },
+    default: true,
+  },
+  {
+    name: 'Portobello',
+    startCoords: [55.95, -3.12],
+    segmentId: '9342203',
+    segDict: {
+    },
+    default: false,
+  },
+  {
+    name: 'Inverness',
+    startCoords: [57.46, -4.24],
+    segmentId: '11829562',
+    segDict: {
+    },
+    default: false,
+  },
+  {
+    name: 'Pollok',
+    startCoords: [55.83, -4.30],
+    segmentId: '18916728',
+    segDict: {
+    },
+    default: false,
+  },
+  {
+    name: 'Fukakitaryokuchi',
+    startCoords: [34.73, 135.63],
+    segmentId: '',
+    segDict: {
+    },
+    default: false,
+  },
+];
+
+export default parkRunDirectory;
