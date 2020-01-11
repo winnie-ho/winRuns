e53ecab2ea1ea294bb379a407b9153eef110b280
Winnie Ho <winnie-ho@hotmail.co.uk>
2020-01-11 17:34:22 +0000

    Getting full range of park runs from directory for the year.

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VueResource from 'vue-resource';
+import parkRunDirectory from '../mixins/parkRunDirectory';
 import { actions } from './actions';
 
 Vue.use(Vuex);
@@ -67,7 +68,26 @@ export const store = new Vuex.Store({
     setActivitiesInPeriod: (state, payload) => (state.activitiesInPeriod = payload),
   },
   getters: {
-    parkRuns: (state) => state.selectedParkRun.startCoords && state.activitiesInPeriod.filter((activity) => activity.start_latitude && activity.start_latitude.toFixed(2) == 55.98 && activity.start_longitude && activity.start_longitude.toFixed(2) == -3.29).sort((a, b) => new Date(b.start_date) - new Date(a.start_date)),
+    parkRuns: (state) => {
+      // -- To get park runs in mid run --
+      // the detailed Park Run contains all known strava segment_efforts of the park run within the time frame of the park run (09:30 to 10:30)
+      const runsOnCorrectDay = state.activitiesInPeriod.filter(activity => activity.type === 'Run' && (
+        new Date(activity.start_date_local).getDay() === 6
+        || (new Date(activity.start_date_local).getMonth() === 11 && new Date(activity.start_date_local).getDate() === 25)
+        || (new Date(activity.start_date_local).getMonth() === 0 && new Date(activity.start_date_local).getDate() === 1)
+
+      ));
+      const finalRuns = [];
+      runsOnCorrectDay.forEach(run => {
+        parkRunDirectory.forEach(parkRunLocation => {
+          if (run.start_latitude.toFixed(2) == parkRunLocation.startCoords[0] && run.start_longitude.toFixed(2) == parkRunLocation.startCoords[1]) {
+            finalRuns.push(run);
+          }
+        });
+      });
+
+      return finalRuns;
+    },
     dateOrderedFullParkRuns: (state) => state.full && state.fullParkRuns.sort((a, b) => new Date(b.start_date) - new Date(a.start_date)),
     kmSessions: (state) => state.activities.length && state.activities.filter((activity) => activity.name.search('5x 1km') !== -1),
     dateOrderedFullKmSessions: (state) => state.fullKmSessions && state.fullKmSessions.sort((a, b) => new Date(b.start_date) - new Date(a.start_date)),
