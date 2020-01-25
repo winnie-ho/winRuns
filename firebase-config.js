e7a50cceb5ccd8e36db52a114094f25808debd7e
Winnie Ho <winnie-ho@hotmail.co.uk>
2020-01-25 21:35:29 +0000

    Extracing the firebase config object to separate file. Updating app url.

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDHundDSRnWjdXyuYKm_nc3C07cbHtwZ_8",
+  authDomain: "win-runs.firebaseapp.com",
+  databaseURL: "https://win-runs.firebaseio.com",
+  projectId: "win-runs",
+  storageBucket: "win-runs.appspot.com",
+  messagingSenderId: "403833347873",
+  appId: "1:403833347873:web:45bca915563b8441"
+};
