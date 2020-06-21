65f9ef1965bd18b7d0e1708741f41a734a9d718d
Winnie Ho <winnie-ho@hotmail.co.uk>
2020-06-21 18:51:10 +0100

    Activity dock logic for displaying kudos, laps and comments.

---
+++ src/components/activityDock/activityDock.vue
@@ -0,0 +1,61 @@
+<template src='./activityDock.html'></template>
+<style scoped src='./activityDock.css'></style>
+
+<script>
+import renderData from '../../mixins/renderData';
+import appData from '../../mixins/appData';
+
+export default {
+  name: 'activityDock',
+  mixins: [renderData, appData],
+  props: ['activity'],
+  data() {
+    return {
+      toggleLaps: false,
+      toggleKudos: false,
+      toggleComments: false,
+    };
+  },
+  methods: {
+    setDockItem(item) {
+      switch (item) {
+        case 'laps':
+          this.toggleLaps = true;
+          this.toggleKudos = false;
+          this.toggleComments = false;
+          break;
+        case 'comments':
+          this.toggleLaps = false;
+          this.toggleKudos = false;
+          this.toggleComments = true;
+          break;
+        case 'kudos':
+          this.toggleLaps = false;
+          this.toggleKudos = true;
+          this.toggleComments = false;
+          break;
+        default:
+          this.toggleLaps = true;
+          this.toggleKudos = false;
+          this.toggleComments = false;
+      }
+    },
+  },
+  computed: {
+    comments() {
+      return this.$store.state.comments;
+    },
+    kudos() {
+      return this.$store.state.kudos;
+    },
+    laps() {
+      return this.$store.state.activity.laps;
+    },
+    lapsCount() {
+      return (
+        this.$store.state.activity.laps && this.$store.state.activity.laps.length
+      );
+    },
+  },
+};
+</script>
