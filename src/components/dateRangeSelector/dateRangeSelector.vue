7b2303d3ba3080dc57a4026e626ebb5d6f858d0c
Winnie Ho <winnie-ho@hotmail.co.uk>
2020-11-14 10:05:02 +0000

    Creating MAF page, removing km Splits Page.

---
+++ src/components/dateRangeSelector/dateRangeSelector.vue
@@ -0,0 +1,22 @@
+<template src='./dateRangeSelector.html'></template>
+<style scoped src='./dateRangeSelector.css'></style>
+
+<script>
+import moment from 'moment';
+import renderData from '../../mixins/renderData';
+import appData from '../../mixins/appData';
+
+export default {
+  name: 'dateRangeSelector',
+  mixins: [renderData, appData],
+  data() {
+    return {
+      selectedDateRange: '',
+    };
+  },
+  methods: {
+  },
+  computed: {
+  },
+};
+</script>
---
+++ src/pages/maf/maf.vue
@@ -0,0 +1,31 @@
+<template src='./maf.html'></template>
+<style scoped src='./maf.css'></style>
+
+<script>
+import navBar from '../../components/navBar/navBar.vue';
+import dateRangeSelector from '../../components/dateRangeSelector/dateRangeSelector.vue';
+import renderData from '../../mixins/renderData';
+import changePage from '../../mixins/changePage';
+
+export default {
+  name: 'maf',
+  components: {
+    navBar,
+    dateRangeSelector,
+  },
+  mixins: [renderData, changePage],
+  data() {
+    return {
+      pageTitle: 'MAF',
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  },
+};
+</script>
